bb53826ff156b557d2eccfb82d555c3415289d6c
ozzyfromspace <float.circuit@gmail.com>
2023-05-08 20:00:41 -0400

    feat: implement dashboard scaffold

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { RedirectType } from "next/dist/client/components/redirect"
 import { redirect } from "next/navigation"
 import { getSession } from "@/features/AuthProvider"
+import Dashboard from "@/features/Dashboard"
 import PetBar from "@/features/PetBar"
 
 import isSessionValid from "./isSessionValid"
@@ -16,6 +17,7 @@ export default async function IndexPage() {
     <section>
       <div className="mt-5">
         <PetBar />
+        <Dashboard />
       </div>
     </section>
   )
